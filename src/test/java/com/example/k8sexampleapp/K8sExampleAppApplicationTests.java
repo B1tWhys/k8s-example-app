f3fe9db40749537a9fed4679260902c294634c0b
bouldersky <skyarnold1@me.com>
2022-02-20 16:51:41 -0700

    Initial commit

---
+++ src/test/java/com/example/k8sexampleapp/K8sExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8sexampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sExampleAppApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
