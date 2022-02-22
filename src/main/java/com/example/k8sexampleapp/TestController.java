666608214d8cacbd0d80773f47d194b269764d67
bouldersky <skyarnold1@me.com>
2022-02-21 20:16:03 -0700

    fix some stuff

--- src/main/java/com/example/k8sexampleapp/TestController.java
+++ src/main/java/com/example/k8sexampleapp/TestController.java
@@ -1,14 +1,19 @@
 package com.example.k8sexampleapp;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("example")
+@AllArgsConstructor
 public class TestController {
+    TestControllerConfig config;
+    
+    
     @GetMapping("/foo")
     public String fooEndpoint() {
-        return "some string";
+        return config.getResponseMesg();
     }
 }
---
+++ src/main/java/com/example/k8sexampleapp/TestControllerConfig.java
@@ -0,0 +1,12 @@
+package com.example.k8sexampleapp;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "test")
+public class TestControllerConfig {
+    String responseMesg;
+}
